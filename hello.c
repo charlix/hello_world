843a53efcc965c940d9e249530e3e05e26903775
zhangchunyi <zhangchunyi@qiku.com>
2016-04-23 16:31:10 +0800

    rm info6

--- hello.c
+++ hello.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[])
 	printf("this is del_test branch 3!\n");
 	printf("this is del_test branch 4!\n");
 	printf("this is del_test branch 5!\n");
-	printf("this is del_test branch 6!\n");
 
 	printf("is turn %d\n", is_turn_num(test_num));
 	printf("is turn %d\n", is_turn_num(test_num_2));
