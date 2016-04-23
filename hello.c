f272a2b9494a68961a2f1744eb3aa483b68ea291
zhangchunyi <zhangchunyi@qiku.com>
2016-04-23 17:17:09 +0800

    add msg hee

--- hello.c
+++ hello.c
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
 	int test_num_2 = 12;
 	printf("hello github!\n");
 	printf("hello !\n");
+	printf("hee !\n");
 
 	printf("this is del_test branch 3!\n");
 	printf("this is del_test branch 4!\n");
